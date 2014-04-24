7ec8d8aa1d7332ad94e5af340c0513d6189f3464
Lance Batson <lancebatsondev@gmail.com>
2014-04-24 13:44:46 -0500

    add menu buttons for zip code search and gps

--- src/edu/apsu/csci4020/batson_kuwonu/weather/MainActivity.java
+++ src/edu/apsu/csci4020/batson_kuwonu/weather/MainActivity.java
@@ -2,11 +2,15 @@ package edu.apsu.csci4020.batson_kuwonu.weather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public TextView mainTemperatureText;
+    private WeatherAPI weatherAPI;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,7 +19,28 @@ public class MainActivity extends Activity {
 
         mainTemperatureText = (TextView) findViewById(R.id.mainTemperatureText);
 
-        WeatherAPI weatherAPI = new WeatherAPI(this);
-        weatherAPI.retrieveWeatherInfo("37043");
+        weatherAPI = new WeatherAPI(this);
+//        weatherAPI.retrieveWeatherInfo("37043");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                return true;
+            case R.id.action_gps:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
