17cfbfb6f7953ecfb116a172932e54014ce3fe04
Lance Batson <lancebatsondev@gmail.com>
2014-04-29 15:55:38 -0500

    daily forecast listview now displaying

---
+++ src/edu/apsu/csci4020/batson_kuwonu/weather/DailyForecast.java
@@ -0,0 +1,84 @@
+package edu.apsu.csci4020.batson_kuwonu.weather;
+
+
+public class DailyForecast {
+
+    private String weekday;
+    private Integer day;
+    private Integer high_f;
+    private Integer low_f;
+    private Integer high_c;
+    private Integer low_c;
+
+    public DailyForecast() {
+    }
+
+    public DailyForecast(String weekday, Integer day, Integer high_f, Integer low_f, Integer high_c, Integer low_c) {
+        this.weekday = weekday;
+        this.day = day;
+        this.high_f = high_f;
+        this.high_c = high_c;
+        this.low_f = low_f;
+        this.low_c = low_c;
+    }
+
+    public String getWeekday() {
+        return weekday;
+    }
+
+    public void setWeekday(String weekday) {
+        this.weekday = weekday;
+    }
+
+    public Integer getDay() {
+        return day;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+    public Integer getHigh_f() {
+        return high_f;
+    }
+
+    public void setHigh_f(Integer high_f) {
+        this.high_f = high_f;
+    }
+
+    public Integer getHigh_c() {
+        return high_c;
+    }
+
+    public void setHigh_c(Integer high_c) {
+        this.high_c = high_c;
+    }
+
+    public Integer getLow_f() {
+        return low_f;
+    }
+
+    public void setLow_f(Integer low_f) {
+        this.low_f = low_f;
+    }
+
+    public Integer getLow_c() {
+        return low_c;
+    }
+
+    public void setLow_c(Integer low_c) {
+        this.low_c = low_c;
+    }
+
+    @Override
+    public String toString() {
+        return "DailyForecast{" +
+                "weekday='" + weekday + '\'' +
+                ", day=" + day +
+                ", high_f=" + high_f +
+                ", low_f=" + low_f +
+                ", high_c=" + high_c +
+                ", low_c=" + low_c +
+                '}';
+    }
+}
--- src/edu/apsu/csci4020/batson_kuwonu/weather/MainActivity.java
+++ src/edu/apsu/csci4020/batson_kuwonu/weather/MainActivity.java
@@ -5,10 +5,15 @@ import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.*;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements SearchDialogFragment.SearchDialogListener, WeatherAPI.WeatherAPIListener {
 
@@ -16,17 +21,28 @@ public class MainActivity extends Activity implements SearchDialogFragment.Searc
     private WeatherAPI.DownloadTask downloadTask;
 
     public TextView mainTemperatureText;
+    public ListView dailyForecastListView;
     public RelativeLayout progressLayout;
+    public ArrayList<DailyForecast> dailyForecastArray;
+    public ArrayAdapter<DailyForecast> dailyForecastArrayAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Initial setup
+        // View setup
         mainTemperatureText = (TextView) findViewById(R.id.tv_main_temp);
+        dailyForecastListView = (ListView) findViewById(R.id.lv_daily_forecast);
         progressLayout = (RelativeLayout) findViewById(R.id.layout_progress);
+
+        // Data setup
         weatherAPI = new WeatherAPI(this);
+        dailyForecastArray = new ArrayList<DailyForecast>();
+        dailyForecastArrayAdapter = new ArrayAdapter<DailyForecast>(this,
+                android.R.layout.simple_list_item_1,
+                dailyForecastArray);
+        dailyForecastListView.setAdapter(dailyForecastArrayAdapter);
     }
 
     @Override
@@ -81,8 +97,59 @@ public class MainActivity extends Activity implements SearchDialogFragment.Searc
                 mainTemperatureText.setText("N/A");
             } else {
                 JSONObject current = responseData.getJSONObject("current_observation");
-                mainTemperatureText.setText(current.getInt("temp_f") + "\u00B0");
+                // Fahrenheit u2109
+                // Celcius u2103
+                mainTemperatureText.setText(current.getInt("temp_f") + "\u2109");
+                displayCurrentConditions(responseData);
+                displayHourlyForecast(responseData);
+                displayDailyForecast(responseData);
             }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayCurrentConditions(JSONObject data) {
+
+    }
+
+    public void displayHourlyForecast(JSONObject data) {
+
+    }
+
+    public void displayDailyForecast(JSONObject data) {
+        try {
+            // Remove data
+            dailyForecastArray.clear();
+
+            // Get forecastday array of objects
+            JSONObject forecast = data.getJSONObject("forecast");
+            JSONObject simpleforecast = forecast.getJSONObject("simpleforecast");
+            JSONArray forecastday = simpleforecast.getJSONArray("forecastday");
+
+            for (int i = 0; i < forecastday.length(); i++) {
+                // Get JSON objects
+                JSONObject row = forecastday.getJSONObject(i);
+                JSONObject date = row.getJSONObject("date");
+                JSONObject high = row.getJSONObject("high");
+                JSONObject low = row.getJSONObject("low");
+
+                // Get necessary values for daily forecast
+                String weekday = date.getString("weekday");
+                int day = date.getInt("day");
+                int high_f = high.getInt("fahrenheit");
+                int high_c  = high.getInt("celsius");
+                int low_f = low.getInt("fahrenheit");
+                int low_c = low.getInt("celsius");
+
+                // Add each day's forecast to list
+                DailyForecast dailyForecast = new DailyForecast(weekday, day, high_f, high_c, low_f, low_c);
+                dailyForecastArray.add(dailyForecast);
+            }
+
+            // Update listview array adapter
+            dailyForecastArrayAdapter.notifyDataSetChanged();
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
